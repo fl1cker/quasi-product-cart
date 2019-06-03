3916f087211cea6341dc8f67419024a9d23e60ee
Aaron <the_flicker7@hotmail.com>
2019-06-02 22:54:38 -0400

    git init.  add toolbar and material icons

---
+++ quasi-product-cart/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderToolbar from './components/HeaderToolbar/HeaderToolbar';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderToolbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ quasi-product-cart/src/components/HeaderToolbar/HeaderToolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeaderToolbar.scss';
+
+const HeaderToolbar = () => {
+    return (
+        <div className="header-toolbar">
+            <div className="shopping-cart">
+                <i className="material-icons">shopping_cart</i>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderToolbar;
