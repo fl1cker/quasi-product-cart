9fc7bb33c4e7a26583ef5c5e75722c87730f31db
Aaron <the_flicker7@hotmail.com>
2019-06-05 22:44:01 -0400

    getting the product list looking right.

--- quasi-product-cart/src/components/CartItem/CartItem.js
+++ quasi-product-cart/src/components/CartItem/CartItem.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import './CartItem.js';
+import './CartItem.scss';
 
-const CartItem = ({item}) => {
+const CartItem = ({ item }) => {
     return (
-        <div>
-            <span>{item.image}</span>
-            <span>| {item.name}</span>
-            <span>| {item.description}</span>
+        <div className="CartItem">
+            <div className="image">
+                {item.image}
+            </div>
+            <div className="details">
+                <div className="price">
+                    ${item.cost.toFixed(2)}
+                </div>
+                <div className="name">
+                    {item.name + ' ' + item.description}
+                </div>
+                <div className="details">
+                    qty: {item.quantity} | {item.details.join(" | ")}
+                </div>
+            </div>
+            <div className="close-button">x</div>
         </div>
-    )
+    );
 }
 
 export default CartItem;
--- quasi-product-cart/src/components/ShoppingCartPreview/ShoppingCartPreview.js
+++ quasi-product-cart/src/components/ShoppingCartPreview/ShoppingCartPreview.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import CartItem from '../CartItem/CartItem';
 import './ShoppingCartPreview.scss';
+import { EDEADLK } from 'constants';
 
 const ShoppingCartPreview = (props) => {
     const sampleItems = [{
         image: 'IMAGE',
+        cost: 8.00,
         name: 'Just Shapes and Beats',
         description: 'Beserk Studios',
-        cost: 5.27
-    }];
+        details: ["digital"],
+        quantity: 1,
+    },
+    {
+        image: 'IMAGE2',
+        cost: 60.00,
+        name: 'Breath Of The Wild',
+        description: 'Nintendo',
+        details: ["physical", "Spec. Ed."],
+        quantity: 1,
+    },
+    {
+        image: 'IMAGE3',
+        cost: 8.00,
+        name: 'Just Shapes and Beats',
+        description: 'Beserk Studios',
+        details: ["digital"],
+        quantity: 1,
+    },];
+    
 
     return (
         <div className="preview-panel">
-            <div>Total Cost: ${sampleItems[0].cost}</div>
-            {sampleItems.map(item => <CartItem item={item}/>)}
-            <button className="submit-button" onClick={() => props.proceedToCheckout()}>View Cart and Checkout</button>
+            <div className="cost-section">
+                <p><span className="bold-text">cart total:</span> ${sampleItems[0].cost.toFixed(2)}</p>
+            </div>
+            <div className="item-list">
+                {sampleItems.map(item => <CartItem className="CartItem" item={item} />)}
+            </div>
+            <div className="checkout-section">
+                <button className="submit-button" onClick={() => props.proceedToCheckout()}>View Cart and Checkout</button>
+            </div>
         </div>
     )
 }
