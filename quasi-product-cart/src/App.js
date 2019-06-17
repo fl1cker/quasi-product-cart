1ed3149bf771738cbd94f3eb6b965fc22def01d5
Aaron <the_flicker7@hotmail.com>
2019-06-17 13:06:18 -0400

    start building out product page

--- quasi-product-cart/src/App.js
+++ quasi-product-cart/src/App.js
@@ -2,6 +2,8 @@ import './App.scss';
 import React from 'react';
 import HeaderToolbar from './components/HeaderToolbar/HeaderToolbar';
 import ShoppingCartPreview from './components/ShoppingCartPreview/ShoppingCartPreview';
+import FilterPanel from './components/FilterPanel/FilterPanel';
+import ProductPage from './components/ProductPage/ProductPage';
 
 const App = () => {
   return (
@@ -9,7 +11,13 @@ const App = () => {
       <div className="HeaderToolbar">
         <HeaderToolbar />
       </div>
-      <div className="body">
+      <aside className="FilterPanel">
+        <FilterPanel />
+      </aside>
+      <div className="ProductPage">
+        <ProductPage />
+      </div>
+      <div className="ShoppingCartPreview">
         <ShoppingCartPreview />
       </div>
     </div>
---
+++ quasi-product-cart/src/components/FilterPanel/FilterPanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FilterPanel.scss'
+
+const FilterPanel = () => {
+    return (
+        <div className="filter-panel-body">My Filter Panel</div>
+    )
+}
+
+export default FilterPanel;
