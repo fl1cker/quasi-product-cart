3b2b532ddef3c08dde1fc7db3306ac83cf0cb5fb
Aaron <the_flicker7@hotmail.com>
2019-06-18 12:51:57 -0400

    css up the product page.  need to wire add to cart

--- quasi-product-cart/src/components/Product/Product.js
+++ quasi-product-cart/src/components/Product/Product.js
@@ -3,7 +3,22 @@ import './Product.scss';
 
 const Product = (props) => {
     return (
-        <div>{props.product.id}</div>
+        <div className="Product-host">
+            <div className="Product-image-container">
+                <img className="Product-image" src={props.product.imageUrl} title={props.product.name} alt={props.product.name}/>
+            </div>
+            <div className="Product-item-container">
+                <div className="Product-item-name-section">
+                    <div className="Product-item-name bold">{props.product.name}</div>
+                    <div className="Product-item-description">{props.product.description}</div>
+                </div>
+                <div className="Product-item-details">Details: {props.product.details.join('  |  ')}</div>
+                <div className="Product-item-price">{props.product.price}</div>
+                <div className="Product-add-button-container">
+                    <button className="Product-add-button shopping-button">Add to cart</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
