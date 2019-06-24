2d7db07362f0f0baaf19be7df16ee7b31467a0a5
Aaron <the_flicker7@hotmail.com>
2019-06-24 13:11:45 -0400

    adding the svg.  it needs work.  work on thickness of person and colors

--- quasi-product-cart/src/components/HeaderToolbar/HeaderToolbar.js
+++ quasi-product-cart/src/components/HeaderToolbar/HeaderToolbar.js
@@ -6,7 +6,9 @@ const HeaderToolbar = (props) => {
     return (
         <div className="header-toolbar">
             <div className="shopping-cart">
-                <i className="material-icons" onMouseEnter={props.showShoppingCart} onMouseOut={props.hideShoppingCart}>shopping_cart</i>
+                {/* <i className="material-icons" onMouseEnter={props.showShoppingCart} onMouseOut={props.hideShoppingCart}>shopping_cart</i> */}
+                <img src="images/icons/shopping-cart.svg" alt="shopping cart icon" className="shopping-cart-svg" onMouseEnter={props.showShoppingCart} onMouseOut={props.hideShoppingCart}></img>
+                
             </div>
         </div>
     );
